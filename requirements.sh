ecc53bb33f3457fdd185be1b1ee79356def40502
Jakub Klama <jakub.klama@gmail.com>
2017-09-20 01:33:29 +0200

    pygobject3 can be found in homebrew

--- requirements.sh
+++ requirements.sh
@@ -4,8 +4,8 @@ case $OSTYPE in
     darwin*)
         echo "I am a Mac.  Brew installing stuff."
         echo "Don't worry about warnings of things already installed."
-        brew install --upgrade cmake glib libusb python3
-        pip3 install cython numpy scipy pygobject
+        brew install --upgrade cmake glib libusb python3 pygobject3
+        pip3 install cython numpy scipy
         ;;
     default)
         echo "Don't know how to set up for your OS."
