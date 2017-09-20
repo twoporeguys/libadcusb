aae60b67c460d1af6f93692b03f1fff88f4b7c63
Dennis Gentry <dennis.gentry@gmail.com>
2017-09-20 16:46:20 -0700

    Move python requirements to their own requirements.txt

--- requirements.sh
+++ requirements.sh
@@ -6,7 +6,7 @@ case $OSTYPE in
         echo "Don't worry about warnings of things already installed."
         brew install cmake glib libusb python3
         brew install pygobject3 --with-python3
-        pip3 install cython numpy scipy
+        pip3 install -r requirements.txt
         ;;
     default)
         echo "Don't know how to set up for your OS."
