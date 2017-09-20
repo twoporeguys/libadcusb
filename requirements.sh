86ab2004b6217025c2e73b82b1301b12f718dac1
Jakub Klama <jakub.klama@twoporeguys.com>
2017-09-21 01:43:32 +0200

    brew install pygobject3 requires "--with-python3"

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,8 @@ case $OSTYPE in
     darwin*)
         echo "I am a Mac.  Brew installing stuff."
         echo "Don't worry about warnings of things already installed."
-        brew install cmake glib libusb python3 pygobject3
+        brew install cmake glib libusb python3
+        brew install pygobject3 --with-python3
         pip3 install cython numpy scipy
         ;;
     default)
